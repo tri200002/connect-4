3051dd0cba8f8d7252ac7e4d5d3caa715ff4e7c1
notme4 <102337330+notme4@users.noreply.github.com>
2022-03-24 21:17:17 -0500

    Update team project.cpp

--- team project.cpp
+++ team project.cpp
@@ -1,40 +1,37 @@
 int main() {
+    // False = ‘X’
+    // True = ‘O’
+    boolean playerTurn = false;
 
-// False = ‘X’
-// True = ‘O’
-boolean playerTurn = false;
 
-
-Scanner kb = new Scanner(System.in);
-char[][] board = { {‘_’, ‘_’, ‘_’, ‘_’, ‘_’, ‘_’, ‘_’},
-    {‘_’, ‘_’, ‘_’, ‘_’, ‘_’, ‘_’, ‘_’}, 
-    {‘_’, ‘_’, ‘_’, ‘_’, ‘_’, ‘_’, ‘_’},
-    {‘_’, ‘_’, ‘_’, ‘_’, ‘_’, ‘_’, ‘_’},
-    {‘_’, ‘_’, ‘_’, ‘_’, ‘_’, ‘_’, ‘_’},
-    {‘_’, ‘_’, ‘_’, ‘_’, ‘_’, ‘_’, ‘_’},
-    {‘_’, ‘_’, ‘_’, ‘_’, ‘_’, ‘_’, ‘_’} };
+    Scanner kb = new Scanner(System.in);
+    char[][] board = { {‘_’, ‘_’, ‘_’, ‘_’, ‘_’, ‘_’, ‘_’},
+                       {‘_’, ‘_’, ‘_’, ‘_’, ‘_’, ‘_’, ‘_’}, 
+                       {‘_’, ‘_’, ‘_’, ‘_’, ‘_’, ‘_’, ‘_’},
+                       {‘_’, ‘_’, ‘_’, ‘_’, ‘_’, ‘_’, ‘_’},
+                       {‘_’, ‘_’, ‘_’, ‘_’, ‘_’, ‘_’, ‘_’},
+                       {‘_’, ‘_’, ‘_’, ‘_’, ‘_’, ‘_’, ‘_’},
+                       {‘_’, ‘_’, ‘_’, ‘_’, ‘_’, ‘_’, ‘_’} };
  
-int choice;
+    int choice;
 
 
-Int counter = 0
-while(counter < 42):
+    int counter = 0
+    while(counter < 42):
         drawBoard(board);
         
-if(playerTurn){
+    if(playerTurn){
         cout << “chose a col between 0 and 6 (inclusive)” << endl;
         choice = kb.nextInt();
         while(choice < 0 || choice > 6) {
-                    cout << “Invalid input: must be between 0 and 6 (inclusive)” << endl;
-    choice = kb.nextInt();
-                }
-
-
-                addPiece(board, choice, playerTurn);
-        } else {
-                choice = AIChoice()
+            cout << “Invalid input: must be between 0 and 6 (inclusive)” << endl;
+            choice = kb.nextInt();
         }
-        Int[] play = addPiece(board, choice, playerTurn); // row and col of play
+    
+   } else {
+        choice = AIChoice()
+   }
+        int[] play = addPiece(board, choice, playerTurn); // row and col of play
 
 
   return 0;
@@ -42,12 +39,12 @@ if(playerTurn){
 
 
 public drawBoard(String[][] b) {
-        for(int r = 0; r < b.length; r++) {
-                for(int c = 0; c < b[r].length; c++) {
-                        cout << “|” << b[r][c]; 
-                }
-                cout << “|” << endl;
+    for(int r = 0; r < b.length; r++) {
+        for(int c = 0; c < b[r].length; c++) {
+            cout << “|” << b[r][c]; 
         }
+        cout << “|” << endl;
+    }
 }
 
 
@@ -62,19 +59,15 @@ public aIChoice(char[][] board, int[] play) {
 
 
 public void addPiece (char[][] board, int choice, boolean playerTurn) {
-   for (int row = 0; row < 6; row++) {
-       if(board[row][choice] == '_') {
-           if(!playerTurn) {
-               board[row][choice] = 'X';
-               break;
-           }
-
-
-           board[row][choice] = 'O';
-           break;
-       }
-   }
-
-
-   playerTurn = !playerTurn;
+    for (int row = 0; row < 6; row++) {
+        if(board[row][choice] == '_') {
+            if(!playerTurn) {
+                board[row][choice] = 'X';
+                break;
+            }
+            board[row][choice] = 'O';
+            break;
+        }
+    }
+    playerTurn = !playerTurn;
 }
