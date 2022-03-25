834551f80818d818bbc08a6ce1b59004675b6361
notme4 <102337330+notme4@users.noreply.github.com>
2022-03-24 21:26:17 -0500

    Update team project.cpp

--- team project.cpp
+++ team project.cpp
@@ -5,13 +5,13 @@ int main() {
     // True = ‘O’
     bool playerTurn = false;
 
-    char[][] board = { {‘_’, ‘_’, ‘_’, ‘_’, ‘_’, ‘_’, ‘_’},
-                       {‘_’, ‘_’, ‘_’, ‘_’, ‘_’, ‘_’, ‘_’}, 
-                       {‘_’, ‘_’, ‘_’, ‘_’, ‘_’, ‘_’, ‘_’},
-                       {‘_’, ‘_’, ‘_’, ‘_’, ‘_’, ‘_’, ‘_’},
-                       {‘_’, ‘_’, ‘_’, ‘_’, ‘_’, ‘_’, ‘_’},
-                       {‘_’, ‘_’, ‘_’, ‘_’, ‘_’, ‘_’, ‘_’},
-                       {‘_’, ‘_’, ‘_’, ‘_’, ‘_’, ‘_’, ‘_’} };
+    char[][] board = { {'_', '_', '_', '_', '_', '_', '_'},
+                       {'_', '_', '_', '_', '_', '_', '_'}, 
+                       {'_', '_', '_', '_', '_', '_', '_'},
+                       {'_', '_', '_', '_', '_', '_', '_'},
+                       {'_', '_', '_', '_', '_', '_', '_'},
+                       {'_', '_', '_', '_', '_', '_', '_'},
+                       {'_', '_', '_', '_', '_', '_', '_'} };
  
     int choice;
 
@@ -21,10 +21,10 @@ int main() {
         drawBoard(board);
         
     if(playerTurn){
-        cout << “chose a col between 0 and 6 (inclusive)” << endl;
+        cout << "chose a col between 0 and 6 (inclusive)" << endl;
         choice = kb.nextInt();
         while(choice < 0 || choice > 6) {
-            cout << “Invalid input: must be between 0 and 6 (inclusive)” << endl;
+            cout << "Invalid input: must be between 0 and 6 (inclusive)" << endl;
             choice = kb.nextInt();
         }
     
@@ -41,9 +41,9 @@ int main() {
 public void drawBoard(String[][] b) {
     for(int r = 0; r < b.length; r++) {
         for(int c = 0; c < b[r].length; c++) {
-            cout << “|” << b[r][c]; 
+            cout << "|" << b[r][c]; 
         }
-        cout << “|” << endl;
+        cout << "|" << endl;
     }
 }
 
